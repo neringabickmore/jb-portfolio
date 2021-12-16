b764328da3fe15a629ccb26fa651cf0e92581b35
neringa <neringa.bickmore@gmail.com>
2021-12-16 15:06:21 +0000

    CHNG/Removed MEDIA_URL settings

--- jb_portfolio/settings.py
+++ jb_portfolio/settings.py
@@ -208,7 +208,6 @@ if 'USE_CLOUDINARY' in os.environ:
     CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
 
     CLOUDINARY_DOMAIN_NAME = os.environ.get('CLOUDINARY_DOMAIN_NAME')
-    MEDIA_URL = f'https://{CLOUDINARY_DOMAIN_NAME}/'
 
     # static files
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
